577b6315f16cc7c1a9681a1a5a071073669e8b2c
Yun-Dayeong <ydiosa98@naver.com>
2021-02-09 16:16:47 +0900

    #13 git ignore

--- back_end/config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "bookmanagement"
-})
-
-module.exports = pool
